2c941add40135b15de0748160f84a25d39ec28ee
Jeremy Spencer <jjs2195@columbia.edu>
2013-03-31 22:24:26 -0400

    added user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '25c18e847d0b61e7921f2de1d01fae27568448d3675c699b4bc167c83697bbb881b429f4c0d5a878485dddb62623992e2bc10123d75652caf5157e058ebbe3de'
