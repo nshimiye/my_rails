15df14ae1d28bc2df171d2f1df80b99cb2fb3f38
Jeremy Spencer <jjs2195@columbia.edu>
2013-04-21 08:02:38 -0400

    added rake task to add course

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,5 +4,12 @@ class Course
   has_and_belongs_to_many :users
   has_many :lectures
   
-  field :title, type: String
+  field :CallNumber, type: Integer
+  field :CourseTitle, type: String
+  field :StartTime, type: String
+  field :EndTime, type: String
+  field :MeetsOn, type: String
+  field :Building, type: String
+  field :Room, type: String
+  field :InstructorName, type: String
 end
